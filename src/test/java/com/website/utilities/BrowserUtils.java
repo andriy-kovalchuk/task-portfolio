dfabf86775cfe2c72f9087d49e8b795a1df854e3
andriy-kovalchuk <and.kovalchuk1991@gmail.com>
2025-01-23 16:04:20 -0600

    browser utilities added

---
+++ src/test/java/com/website/utilities/BrowserUtils.java
@@ -0,0 +1,35 @@
+package com.website.utilities;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BrowserUtils {
+
+    /**
+     * reusable browser utilities methods; already solved for synchronization issues
+     * */
+
+    public static void goTo(String url){
+        Driver.getDriver().get(url);
+    }
+
+    public static void clickElement(WebElement webElement){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+        webElement.click();
+    }
+
+    public static void enterText(WebElement webElement, String textToEnter){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.elementToBeClickable(webElement));
+        webElement.clear();
+        webElement.sendKeys(textToEnter);
+    }
+
+    public static String getElementsText(WebElement webElement){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+        wait.until(ExpectedConditions.visibilityOf(webElement));
+        return webElement.getText();
+    }
+}
