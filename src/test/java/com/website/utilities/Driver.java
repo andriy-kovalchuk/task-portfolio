7fe8ad2d68c4a3053e1507d5831587c745e08560
andriy-kovalchuk <and.kovalchuk1991@gmail.com>
2025-01-23 15:15:05 -0600

    driver setup added

---
+++ src/test/java/com/website/utilities/Driver.java
@@ -0,0 +1,59 @@
+package com.website.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Driver {
+
+    private Driver(){}
+
+    private static WebDriver driver;
+
+
+    public static WebDriver getDriver(){
+        if(driver == null){
+            String browserType = ConfigurationReader.BROWSER;
+
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "safari":
+                    WebDriverManager.safaridriver().setup();
+                    driver = new SafariDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            }
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver(){
+        if(driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+    public static void closeDriver(){
+        if(driver != null){
+            driver.close();
+            driver = null;
+        }
+    }
+
+}
